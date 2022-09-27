09b1ae789259f3af128aad898c5a0678931ecb4b
Joe Hernandez <joe.hernandez@instructure.com>
2022-09-26 22:01:25 -0500

    Completed section 5 lesson 57

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
@@ -10,6 +10,8 @@ import HomePage from "../../features/home/HomePage";
 import 'react-toastify/dist/ReactToastify.css'
 
 import Header from "./Header";
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 
 function App() {
   const [isDarkMode, setDarkMode] = useState(false)
@@ -33,11 +35,15 @@ function App() {
       <CssBaseline />
       <Header isDarkMode={isDarkMode} handleOnChange={handleDarkModeChange} />
       <Container>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/catalog' component={Catalog} />
-        <Route path='/catalog/:id' component={ProductDetails} />
-        <Route path='/about' component={AboutPage} />
-        <Route path='/contact' component={ContactPage} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/catalog' component={Catalog} />
+          <Route path='/catalog/:id' component={ProductDetails} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/contact' component={ContactPage} />
+          <Route path='/server-error' component={ServerError} />
+          <Route component={NotFound} />
+        </Switch>
       </Container>
     </ThemeProvider>
   );
